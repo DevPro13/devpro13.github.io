9a4b334176bec53eba6abaec93cfe422d980a9c9
devpro13 <devrajparajuli38@gmail.com>
2026-05-19 22:38:07 +0545

    Visitor map updated

--- src/VisitorMap.jsx
+++ src/VisitorMap.jsx
@@ -3,6 +3,7 @@ import "./VisitorMap.css";
 export default function VisitorMap() {
   return (
     <section className="vm-section">
-      <script type='text/javascript' id='mapmyvisitors' src='https://mapmyvisitors.com/map.js?cl=c8f064&w=800&t=tt&d=ZquOC4ZbDB5R5cx5IUEn6PlrBWbXw0BBUxV_j_7Fg5w&co=231a1a&cmo=8340c9&cmn=231b96&ct=f0ede6'></script></section>
+     <script type='text/javascript' id='mapmyvisitors' src='https://mapmyvisitors.com/map.js?cl=c8f064&w=800&t=n&d=JiQN1_lmLDhBOHFed_0A-8QNZwdSTdKSiXwCOLQ8-Dk&co=1e1b1b'></script>
+     </section>
   );
 }
